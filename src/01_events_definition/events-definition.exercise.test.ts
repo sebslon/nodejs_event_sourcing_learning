e17c34ca71b42a4dc6d8457c97f5212dc9e3876c
Sebastian Sloniec <sebastian.sloniec@gmail.com>
2024-05-27 16:22:20 +0200

    Ex.1 - Events Definition - implement basic events

--- src/01_events_definition/events-definition.exercise.test.ts
+++ src/01_events_definition/events-definition.exercise.test.ts
@@ -1,11 +1,72 @@
-// 1. Define your events and entity here
+import {
+  ProductItemAddedToShoppingCart,
+  ProductItemRemovedFromShoppingCart,
+  ShoppingCartCancelled,
+  ShoppingCartConfirmed,
+  ShoppingCartOpened,
+} from '#core/cart/events';
+import { PricedProductItem } from '#core/cart/product-item.interface';
+import { ShoppingCartEvent } from '#core/cart/shopping-cart.event.type';
+import { v4 as uuid } from 'uuid';
 
 describe('Events definition', () => {
   it('all event types should be defined', () => {
-    const events = [
-      // 2. Put your sample events here
-    ];
+    const shoppingCartId = uuid();
+    const clientId = uuid();
+    const product: PricedProductItem = {
+      productId: uuid(),
+      quantity: 1,
+      unitPrice: 100,
+    };
 
+    const cartOpenedEvent: ShoppingCartOpened = {
+      type: 'ShoppingCartOpened',
+      data: {
+        shoppingCartId,
+        clientId,
+        openedAt: new Date(),
+      },
+    };
+
+    const productItemAddedEvent: ProductItemAddedToShoppingCart = {
+      type: 'ProductItemAddedToShoppingCart',
+      data: {
+        shoppingCartId,
+        productItem: product,
+      },
+    };
+
+    const productItemRemovedEvent: ProductItemRemovedFromShoppingCart = {
+      type: 'ProductItemRemovedFromShoppingCart',
+      data: {
+        shoppingCartId,
+        productItem: product,
+      },
+    };
+
+    const cartConfirmedEvent: ShoppingCartConfirmed = {
+      type: 'ShoppingCartConfirmed',
+      data: {
+        shoppingCartId,
+        confirmedAt: new Date(),
+      },
+    };
+
+    const cartCancelledEvent: ShoppingCartCancelled = {
+      type: 'ShoppingCartCancelled',
+      data: {
+        shoppingCartId,
+        cancelledAt: new Date(),
+      },
+    };
+
+    const events: ShoppingCartEvent[] = [
+      cartOpenedEvent,
+      productItemAddedEvent,
+      productItemRemovedEvent,
+      cartConfirmedEvent,
+      cartCancelledEvent,
+    ];
     expect(events.length).toBe(5);
   });
 });
---
+++ src/core/cart/events/index.ts
@@ -0,0 +1,5 @@
+export * from './product-item-added-to-shopping-cart.event';
+export * from './product-item-removed-from-shopping-cart.event';
+export * from './shopping-cart-cancelled.event';
+export * from './shopping-cart-confirmed.event';
+export * from './shopping-cart-opened.event';
